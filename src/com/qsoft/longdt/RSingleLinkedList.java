3778bc41bca50879e472b579f9249d371c65d96e
ribbon-xx <mr.ribbon@gmail.com>
2013-07-18 13:48:04 +0700

    Step 1 GREEN

--- src/com/qsoft/longdt/RSingleLinkedList.java
+++ src/com/qsoft/longdt/RSingleLinkedList.java
@@ -1,6 +1,19 @@
 package com.qsoft.longdt;
 
+import java.util.LinkedList;
+
 public class RSingleLinkedList {
+
+	private LinkedList<Object> singleLinkedList;
+
+	public RSingleLinkedList() {
+		super();
+		singleLinkedList = new LinkedList<>();
+	}
+
+	public Object size() {
+		return singleLinkedList.size();
+	}
 
 	/**
 	 * @param args
@@ -9,10 +22,4 @@ public class RSingleLinkedList {
 		// TODO Auto-generated method stub
 
 	}
-
-	public Object size() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
